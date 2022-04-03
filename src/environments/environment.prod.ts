131dd6ea080d277911327009750e6102d085f705
Ali Jbara <A.jbara@bsynchro.com>
2022-04-04 01:47:59 +0300

    Added prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  name: 'prod',
+  production: true,
+  defaultLcid: 1033,
+  location: 'LB',
+  Portal: 'https://portal.askbasma.com',
+  IdentityServer: 'https://safenet.identity-staging-mt.askbasma.com',
+  DataLists: 'https://safenet.cora-api-staging-mt.askbasma.com/data-list',
+  CRM: 'https://crm.askbasma.com',
+  GlobalVariables: 'https://safenet.cora-api-staging-mt.askbasma.com/global-variable'
 };
